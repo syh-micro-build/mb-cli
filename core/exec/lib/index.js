ed12d2f6b6565119bf74d8714d7bee440c71d9c4
huishanyi <1342088650@qq.com>
2023-12-07 16:34:27 +0800

    子项目 exec 添加根据命令选择相关命令包

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,8 +1,30 @@
 'use strict';
 
+const Package = require('@mb-cli/Package');
+const log = require('@mb-cli/log');
+
+const SETTINGS = {
+  init: '@mb-cli/init'
+}
 
 function exec() {
-  console.log(process.env.CLI_TARGET_PATH);
+  const targetPath = process.env.CLI_TARGET_PATH;
+  const homePath = process.env.CLI_HOME_PATH;
+
+  log.verbose('targetPath',targetPath)
+  log.verbose('homePath',homePath)
+
+  const cmdObj = arguments[arguments.length - 1];
+  const cmdName = cmdObj.name();
+  const packageName = SETTINGS[cmdName];
+  const packageVersion = 'latest';
+
+  const pkg = new Package({
+    targetPath,
+    packageName,
+    packageVersion
+  });
+  console.log(pkg);
 }
 
 module.exports = exec;
