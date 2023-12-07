3758aa76ae65fe6c25c19802f87b6eda0fd3c669
huishanyi <1342088650@qq.com>
2023-12-07 16:30:45 +0800

    子项目utils添加类型判断方法

--- utils/utils/lib/index.js
+++ utils/utils/lib/index.js
@@ -1,8 +1,14 @@
 'use strict';
 
-module.exports = utils;
-
-function utils() {
-  console.log('Hello from utils!!!');
-  return 'Hello from utils';
+/**
+ * 判断数据类型是否为对象
+ * @param {any} o 所需类型判断的数据
+ * @returns 布尔值
+ */
+function isObject(o) {
+  return Object.prototype.toString.call(o) === '[object Object]';
 }
+
+module.exports = {
+  isObject
+};
