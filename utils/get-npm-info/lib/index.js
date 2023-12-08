b7dc34df18d26a5335cda56d37a7a54d5df920c6
huishanyi <1342088650@qq.com>
2023-12-08 16:56:11 +0800

    子项目 get-npm-info 添加 JSDoc

--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -4,6 +4,12 @@ const axios = require('axios');
 const urlJoin = require('url-join');
 const semver = require('semver');
 
+/**
+ * 获取 npm 包信息
+ * @param {string} npmName npm 包名称
+ * @param {string} registry npm 源
+ * @returns npm 包信息
+ */
 function getNpmInfo(npmName, registry) {
   if (!npmName) {
     return null;
@@ -20,10 +26,21 @@ function getNpmInfo(npmName, registry) {
   })
 }
 
+/**
+ * 获取默认npm 源
+ * @param {boolean} isOriginal 是否为官方源
+ * @returns 官方源 或 淘宝源
+ */
 function getDefaultRegistry(isOriginal = false) {
   return isOriginal ? 'https://registry.npmjs.org' : 'https://registry.npm.taobao.org';
 }
 
+/**
+ * 获取 npm 版本号集合
+ * @param {string} npmName npm 包名称
+ * @param {string} registry npm 源
+ * @returns 版本号集合
+ */
 async function getNpmVersions(npmName, registry) {
   const data = await getNpmInfo(npmName, registry);
   if (data) {
@@ -33,12 +50,25 @@ async function getNpmVersions(npmName, registry) {
   }
 }
 
+/**
+ * 获取符合 npm 依赖规则的版本号集合
+ * @param {string} baseVersion 基础版本（package.json 中显示的版本）
+ * @param {string} versions 版本号集合
+ * @returns 符合 npm 依赖规则的版本号集合，并从大到小排列，例如：符合 ^1.0.0 的包为 ['1.2.0','1.1.0','1.0.1',...]
+ */
 function getSemverVersions(baseVersion, versions) {
   return versions
     .filter(version => semver.satisfies(version, `^${baseVersion}||~${baseVersion}`))
     .sort((a, b) => semver.gt(b, a));
 }
 
+/**
+ * 获取指定 npm 包的最新版本号
+ * @param {string} baseVersion 基础版本（package.json 中显示的版本）
+ * @param {string} npmName npm 包名称
+ * @param {string} registry npm 源
+ * @returns npm 仓库中指定包的最新版本号
+ */
 async function getNpmSemverVersion(baseVersion, npmName, registry) {
   const versions = await getNpmVersions(npmName, registry);
   const newVersions = getSemverVersions(baseVersion, versions);
@@ -51,5 +81,6 @@ async function getNpmSemverVersion(baseVersion, npmName, registry) {
 module.exports = {
   getNpmInfo,
   getNpmVersions,
-  getNpmSemverVersion
+  getNpmSemverVersion,
+  getDefaultRegistry
 }
