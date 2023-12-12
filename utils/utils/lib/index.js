d32066c8158a12c0aa756f57af61d825b4db3fd8
huishanyi <1342088650@qq.com>
2023-12-12 16:51:23 +0800

    子项目 utils 添加 isArguments 方法

--- utils/utils/lib/index.js
+++ utils/utils/lib/index.js
@@ -9,6 +9,16 @@ function isObject(o) {
   return Object.prototype.toString.call(o) === '[object Object]';
 }
 
+/**
+ * 判断数据类型是否为参数对象
+ * @param {any} o 所需类型判断的数据
+ * @returns 布尔值
+ */
+function isArguments(o) {
+  return Object.prototype.toString.call(o) === '[object Arguments]';
+}
+
 module.exports = {
-  isObject
+  isObject,
+  isArguments
 };
