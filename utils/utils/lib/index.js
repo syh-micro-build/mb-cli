c37bece580d320d233acab1864512ee245114111
huishanyi <1342088650@qq.com>
2023-12-28 15:01:34 +0800

    子项目 utils 添加加载动画方法

--- utils/utils/lib/index.js
+++ utils/utils/lib/index.js
@@ -18,7 +18,27 @@ function isArguments(o) {
   return Object.prototype.toString.call(o) === '[object Arguments]';
 }
 
+/**
+ * 加载动画
+ * @param {string} message 加载动画提示信息
+ * @param {string} spinnerString 加载动画样式，默认为 '|/-\\'
+ * @returns 加载动画对象
+ * @example
+ * const spinner = spinnerStart('正在下载...');
+ * ...
+ * spinner.stop(true);
+ */
+function spinnerStart(message, spinnerString = '|/-\\') {
+  const Spinner = require('cli-spinner').Spinner;
+
+  const spinner = new Spinner(message + ' %s');
+  spinner.setSpinnerString(spinnerString);
+  spinner.start();
+  return spinner;
+}
+
 module.exports = {
   isObject,
-  isArguments
+  isArguments,
+  spinnerStart
 };
