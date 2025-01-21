5a0b52603b51f7f210c18ac22fc10c126e68ab7a
shitao <1426591905@qq.com>
2025-01-21 11:12:02 +0800

    feat(api): :sparkles: 提交生成基础方法

--- packages/cli/lib/create.ts
+++ packages/cli/lib/create.ts
@@ -1,5 +1,39 @@
-const initCreate = (name: string): void => {
-  console.log(`create ${name}`);
+import chalk from "chalk";
+import fs from "fs-extra";
+import path from "path";
+import validateProjectName from "validate-npm-package-name";
+
+import { generator } from "./generator";
+
+/**
+ * 创建项目
+ * @param {string} projectName - 项目名称
+ * @returns {Promise<void>}
+ */
+const create = async (
+  projectName: string = generator.baseOptions.projectName
+): Promise<void> => {
+  const result = validateProjectName(projectName);
+  if (!result.validForNewPackages) {
+    console.error(chalk.red(`Invalid project name: "${name}"`));
+    result.errors &&
+      result.errors.forEach((err: string) => {
+        console.error(chalk.red.dim("Error: " + err));
+      });
+    result.warnings &&
+      result.warnings.forEach((warn: string) => {
+        console.error(chalk.red.dim("Warning: " + warn));
+      });
+    process.exit(1);
+  }
+
+  const cwd = generator.baseOptions.baseUrl;
+  generator.baseOptions.projectName = projectName;
+
+  const targetDir = path.resolve(cwd, projectName || ".");
+  if (fs.existsSync(targetDir)) {
+    fs.removeSync(targetDir);
+  }
 };
 
-export default initCreate;
+export default create;
