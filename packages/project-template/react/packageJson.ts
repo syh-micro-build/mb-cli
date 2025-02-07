d4c45eba32c3e4ff64b4b59166e2879460790bc0
shitao <1426591905@qq.com>
2025-02-07 18:48:19 +0800

    feat(ui): :sparkles: 提交模版

--- packages/project-template/react/packageJson.ts
+++ packages/project-template/react/packageJson.ts
@@ -1,14 +1,14 @@
 export default {
-  'default-react':{
-        dependencies: {
-            vue: "^3.5.13"
-          },
-          devDependencies: {
-            "@vitejs/plugin-vue": "^5.2.1",
-            "@vue/tsconfig": "^0.7.0",
-            typescript: "~5.6.2",
-            vite: "^6.0.5",
-            "vue-tsc": "^2.2.0"
-          }
+  "default-react": {
+    dependencies: {
+      vue: "^3.5.13"
     },
-}
+    devDependencies: {
+      "@vitejs/plugin-vue": "^5.2.1",
+      "@vue/tsconfig": "^0.7.0",
+      typescript: "~5.6.2",
+      vite: "^6.0.5",
+      "vue-tsc": "^2.2.0"
+    }
+  }
+};
