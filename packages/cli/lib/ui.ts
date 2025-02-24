065872bcbe71b411cbff8e84200fdfbdff0312fc
shitao <1426591905@qq.com>
2025-02-24 15:25:33 +0800

    fix(ui): :bug: 添加ui 启动

--- packages/cli/lib/ui.ts
+++ packages/cli/lib/ui.ts
@@ -1,15 +1,17 @@
+import { startServer } from "@mb-cli/cli-server";
 import { getProjectRootPath } from "@mb-cli/utils";
 import path from "path";
 import { createServer } from "vite";
 
 const startUi = async (): Promise<void> => {
   const rootDir = await getProjectRootPath();
+  await startServer();
   const docs = path.join(rootDir, `/web/ui`);
 
   const server = await createServer({
     configFile: path.resolve(docs, "vite.config.ts"),
     server: {
-      port: 3000
+      port: 8080
     },
     root: docs
   });
